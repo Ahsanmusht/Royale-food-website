52697a1d016a027cb57b2d54f7b8bd6b11e2c616
Ahsanmusht <ahsanmushtaq901@gmail.com>
2022-07-27 18:16:59 +0500

    code

---
+++ index.js
@@ -0,0 +1,26 @@
+
+let menu = document.querySelector('#menu');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+
+// swiper slider
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+  });
